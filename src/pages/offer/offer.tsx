5e74413fb2ddf0134a954ac4f91348c6a9678e82
alex-kabanin <alexcobainius@gmail.com>
2024-11-26 20:34:05 +0900

    update offer 2

--- src/pages/offer/offer.tsx
+++ src/pages/offer/offer.tsx
@@ -183,7 +183,7 @@ export default function Offer () {
                       </svg>
                     </label>
                   </div>
-                  <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" defaultValue={""} />
+                  <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" defaultValue={''} />
                   <div className="reviews__button-wrapper">
                     <p className="reviews__help">
                       To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
